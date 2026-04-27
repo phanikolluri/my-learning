ebc23de0ee6b0d835cfe5ea983bcc4afa303be62
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-04-27 16:02:00 +0530

    learning terraform from foundation

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "my_instance" {
   vpc_security_group_ids = var.security_group_id
   user_data = <<EOF
 #!/bin/bash
-yum update -y
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
