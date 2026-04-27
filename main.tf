8f73e82e68ba4820aca1e862f7a65bc34962070f
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-04-27 12:22:12 +0530

    learning terraform from foundation

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "main" {
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "my_workstation"
+  }
+}
+
+
+
