b45f1c92592bd8de7b68bd6c54969be2f32cd63f
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2026-04-27 12:41:06 +0530

    learning terraform from foundation

--- main.tf
+++ main.tf
@@ -26,6 +26,13 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+EOF
 
   tags = {
     Name = "my_workstation"
